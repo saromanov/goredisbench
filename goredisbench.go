14253a1f8e649c669c3a981a969ec006058bb1b9
Sergey Romanov <xxsmotur@gmail.com>
2015-04-25 21:37:37 +0500

    Append average result

--- goredisbench.go
+++ goredisbench.go
@@ -1,20 +1,25 @@
 package goredisbench
 
-import
-(
-	"github.com/fzzy/radix/redis"
-	"time"
+import (
 	"fmt"
+	"github.com/fzzy/radix/redis"
 	"log"
+	"time"
 )
 
-
-type Goredisbench struct{
-	client* redis.Client
+//Goredisbench creates
+type Goredisbench struct {
+	client   *redis.Client
 	commands []string
 }
 
-func Init()*Goredisbench {
+type Options struct {
+	//Show average time of running command after avgtimes times
+	Avgtimes int
+}
+
+//Init creates and returns object of Goredisbench structure
+func Init() *Goredisbench {
 	grb := new(Goredisbench)
 	clinet, err := redis.DialTimeout("tcp", "127.0.0.1:6379", time.Duration(10)*time.Second)
 	if err != nil {
@@ -24,7 +29,8 @@ func Init()*Goredisbench {
 	return grb
 }
 
-func (grb*Goredisbench) AddCommands(commands []string) {
+//AddCommands is append command(for example hmset) to benchmarking
+func (grb *Goredisbench) AddCommands(commands []string) {
 	if len(commands) > 0 {
 		grb.commands = commands
 	} else {
@@ -32,19 +38,55 @@ func (grb*Goredisbench) AddCommands(commands []string) {
 	}
 }
 
-func (grb*Goredisbench) Start(iters [] int){
-	start := time.Now()
+func (grb *Goredisbench) Start(iters []int, opt ...Options) {
+	if len(opt) > 0 && opt[0].Avgtimes > 0 {
+		grb.runWithAvg(iters, opt[0].Avgtimes)
+	} else {
+		grb.run(iters)
+	}
+}
+
+func (grb *Goredisbench) run(iters []int) {
 	for _, command := range grb.commands {
 		comm := fmt.Sprintf("Command: %s", command)
 		fmt.Println(comm)
 		for _, it := range iters {
-			for i := 0; i < it; i++ {
-				item := fmt.Sprintf("%s%i%i", command, it, i)
-				grb.client.Cmd("hmset", item, "fun")
-			}
+			comm := fmt.Sprintf("Number of iterations: %d", it)
+			fmt.Println(comm)
+			result := grb.loop(it, command, true)
+			fmt.Println("Result: ", result)
 		}
 	}
-	end := time.Since(start)
-	fmt.Println("Result: ", end.Seconds())
+}
 
+func (grb *Goredisbench) loop(it int, command string, showitmessage bool) float64 {
+	start := time.Now()
+	if showitmessage {
+		itnumber := fmt.Sprintf("Number of iterations: %d", it)
+		fmt.Println(itnumber)
+	}
+	for i := 0; i < it; i++ {
+		item := fmt.Sprintf("%s%d%d", command, it, i)
+		grb.client.Cmd("hmset", item, "fun")
+	}
+	end := time.Since(start)
+	return end.Seconds()
+}
+
+func (grb *Goredisbench) runWithAvg(iters []int, avgtimes int) {
+	for _, command := range grb.commands {
+		comm := fmt.Sprintf("Command: %s", command)
+		fmt.Println(comm)
+		for _, it := range iters {
+			preresult := fmt.Sprintf("Average result after %d times of %d iterations", avgtimes, it)
+			fmt.Println(preresult)
+			avgvalue := 0.0
+			for i := 0; i < avgtimes; i++ {
+				result := grb.loop(it, command, false)
+				avgvalue += result
+			}
+			fmt.Println("Result: ", avgvalue/float64(avgtimes))
+		}
+
+	}
 }
